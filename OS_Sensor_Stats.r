3e98509e2eb4acc371fb3acf0cbc39d6774af944
Stephanie Fulton <stephgfulton@gmail.com>
2021-12-06 16:07:49 -0800

    Update OS_Sensor_Stats.r

--- OS_Sensor_Stats.r
+++ OS_Sensor_Stats.r
@@ -1,6 +1,15 @@
 # for doing stats on OS sensor data
 
+# for generating overall OS sensor accuracy (Atlas vs. "Other")
+
 accur.dat = read.csv(file = "Mean_Error.csv",stringsAsFactors = F)
 str(accur.dat)
 
 t.test(x = accur.dat$Mean_Error[grep(pattern = "Atlas",x = accur.dat$Sensor_Group)],y = accur.dat$Mean_Error[grep(pattern = "Other",x = accur.dat$Sensor_Group)])
+
+# for generating overall OS sensor accuracy (Atlas vs. "Other")
+
+precis.dat = read.csv(file = "Mean_Deviation.csv",stringsAsFactors = F)
+str(precis.dat)
+
+t.test(x = precis.dat$Mean_Deviation[grep(pattern = "Atlas",x = precis.dat$Sensor_Group)],y = precis.dat$Mean_Deviation[grep(pattern = "Other",x = precis.dat$Sensor_Group)])
